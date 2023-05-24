3f9a8c1ff3c410804927435f17c134f92a93a2da
djonnybravo <djonnybravo29@gmail.com>
2023-05-24 23:00:42 +0300

    feat: add conditional rendering in component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import {AppBar, Button, Grid, Toolbar} from "@material-ui/core";
+import {NavLink} from "react-router-dom";
+import {LOGIN_ROUTE} from "../utils/consts";
 
 const Navbar = () => {
+    const user = true
     return (
         <AppBar position="static" color={"secondary"}>
             <Toolbar>
-                <Grid container justify={"flex-end"}>
-                        <Button variant={"outlined"}>Логин</Button>
+                <Grid container justifyContent={"flex-end"}>
+                    {user ?
                         <Button variant={"outlined"}>ВЫйти</Button>
+                        :
+                        <NavLink to={LOGIN_ROUTE}>
+                            <Button variant={"outlined"}>Логин</Button>
+                        </NavLink>
+                    }
                 </Grid>
             </Toolbar>
         </AppBar>
