752fff3d6b21f2b468d94c3fffcbc5bb815a362a
djonnybravo <djonnybravo29@gmail.com>
2023-05-24 22:13:04 +0300

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
 import './App.css';
+import {BrowserRouter} from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Navbar/>
+        </BrowserRouter>
+    );
 }
 
 export default App;
