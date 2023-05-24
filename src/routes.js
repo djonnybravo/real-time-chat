42bcf6d5e5caa26b138bda4204d0c96a53cb56c4
djonnybravo <djonnybravo29@gmail.com>
2023-05-24 22:10:01 +0300

    feat: create routes

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import {CHAT_ROUTE, LOGIN_ROUTE} from "./utils/consts";
+import Login from "./components/Login";
+import Chat from "./components/chat";
+
+export const publicRoutes = [
+    {
+        path: LOGIN_ROUTE,
+        component: Login
+    }
+]
+
+export const privateRoutes = [
+    {
+        path: CHAT_ROUTE,
+        component: Chat
+    }
+]
