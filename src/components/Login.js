d667be8b13a48eeb513b4f217a3c380d7c6210cb
djonnybravo <djonnybravo29@gmail.com>
2023-05-24 23:18:53 +0300

    feat: crete login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {Box, Button, Container, Grid} from "@material-ui/core";
 
 const Login = () => {
     return (
-        <div>
-            Login
-        </div>
+        <Container>
+            <Grid style={{height: window.innerHeight - 50}} container justifyContent={"center"} alignItems={"center"}>
+                <Grid style={{width: 400, height: 200, background:"lightgray" }}  container direction={"column"} alignItems={"center"} justifyContent={"center"}>
+                    <Box p={5}>
+                        <Button variant={"outlined"}>Войти с помощью GOOGLE</Button>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 
