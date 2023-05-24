9f4a9022c4b9973af9e9ac66147f293fc9fa062d
djonnybravo <djonnybravo29@gmail.com>
2023-05-24 22:55:43 +0300

    feat: add NavBar from MUI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {AppBar, Button, Grid, Toolbar} from "@material-ui/core";
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <AppBar position="static" color={"secondary"}>
+            <Toolbar>
+                <Grid container justify={"flex-end"}>
+                        <Button variant={"outlined"}>Логин</Button>
+                        <Button variant={"outlined"}>ВЫйти</Button>
+                </Grid>
+            </Toolbar>
+        </AppBar>
     );
 };
 
