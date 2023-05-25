2f1245336f26e6383dc4b8497210bc33cb861f10
djonnybravo <djonnybravo29@gmail.com>
2023-05-25 14:03:45 +0300

    fix:  AppRouter.js and Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {AppBar, Button, Grid, Toolbar} from "@material-ui/core";
 import {NavLink} from "react-router-dom";
 import {LOGIN_ROUTE} from "../utils/consts";
+import {Context} from "../index";
+import {useAuthState} from "react-firebase-hooks/auth";
 
 const Navbar = () => {
-    const user = true
+    const {auth} = useContext(Context)
+    const [user] = useAuthState(auth)
+
+    const signOut = () => auth.signOut()
+
     return (
         <AppBar position="static" color={"secondary"}>
             <Toolbar variant={"dense"}>
                 <Grid container justifyContent={"flex-end"}>
                     {user ?
-                        <Button variant={"outlined"}>ВЫйти</Button>
+                        <Button variant={"outlined"} onClick={signOut}>ВЫйти</Button>
                         :
                         <NavLink to={LOGIN_ROUTE}>
                             <Button variant={"outlined"}>Логин</Button>
