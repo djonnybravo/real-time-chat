f218089e5b6aef17825d94563b5496999fdd3aec
djonnybravo <djonnybravo29@gmail.com>
2023-05-25 13:59:11 +0300

    feat:  firebase login with google auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,24 @@
-import React, {useContext} from 'react';
+ import React, {useContext} from 'react';
 import {Box, Button, Container, Grid} from "@material-ui/core";
+ import {Context} from "../index";
+ import firebase from "firebase/compat/app";
 
 const Login = () => {
 
     const {auth} = useContext(Context)
+
+    const login = async () => {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        const {user} = await auth.signInWithPopup(provider)
+        console.log(user)
+    }
 
     return (
         <Container>
             <Grid style={{height: window.innerHeight - 50}} container justifyContent={"center"} alignItems={"center"}>
                 <Grid style={{width: 400, height: 200, background:"lightgray" }}  container direction={"column"} alignItems={"center"} justifyContent={"center"}>
                     <Box p={5}>
-                        <Button variant={"outlined"}>Войти с помощью GOOGLE</Button>
+                        <Button variant={"outlined"} onClick={login}>Войти с помощью GOOGLE</Button>
                     </Box>
                 </Grid>
             </Grid>
